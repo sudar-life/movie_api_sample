8ffc8f005b70d05314b3fa4b7d69a731648cecd8
sudar-life <mousai86@gmail.com>
2021-06-23 21:55:40 +0900

    무한 재귀 함수 삭제

--- lib/src/controller/movie_controller.dart
+++ lib/src/controller/movie_controller.dart
@@ -24,6 +24,6 @@ class MovieController extends ChangeNotifier {
 
   void _loadMovieListWithGenre() async {
     movies = await _movieRepository.loadMovieListWithGenre(activeGenreId);
-    _loadMovieListWithGenre();
+    notifyListeners();
   }
 }
